93dffba2a835fb7b26a92268076cc6b0de0bc72a
Digamer001 <diegobezerra864@gmail.com>
2026-09-12 16:12:57 -0300

    fix: ajuste de rota relativa para erro 404

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante caminhos relativos para os arquivos estáticos
-})  
+  base: './', // Garante que a busca dos arquivos ocorra na pasta relativa correta
+});   
