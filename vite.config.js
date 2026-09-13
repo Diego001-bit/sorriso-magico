08f6713bd257b49e3f732a8a0ef45dbb742989d4
Digamer001 <diegobezerra864@gmail.com>
2026-09-13 00:33:24 -0300

    VITE.CONFIG

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante que a busca dos arquivos ocorra na pasta relativa correta
-});   
+  base: './',
+});
